0dbba5a3024b108dcb6f95bc0c796fd99080ae04
SnowCheetos <cassfunen@yahoo.com>
2022-11-18 11:59:41 -0600

    fixed knn regression

--- neighbors/regression.py
+++ neighbors/regression.py
@@ -15,16 +15,15 @@ class KNNRegressor:
         return np.linalg.norm(self.X - X, axis=1)
 
     def softmax(self, x):
-        max_x = np.amax(x, 1).reshape(x.shape[0],1)
-        e_x = np.exp(x - max_x)
-        return e_x / e_x.sum(axis=1, keepdims=True) 
+        e_x = np.exp(x - x.max())
+        return e_x / e_x.sum() 
 
     def predict(self, X):
         outputs = []
         for x in X:
             dists = self.distance(x)
             idx = dists.argsort()[:self.k]
-            w = dists[idx] / dists[idx].max()
+            w = self.softmax(dists[idx] / dists[idx].max())
             ys = self.y[idx]
             outputs.append(w @ ys)
         return outputs
